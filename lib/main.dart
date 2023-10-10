329f71656d2ee9dd52d66e18ef1dbfffbfedf623
Antpapa99 <anthony.oram@outlook.com>
2023-10-10 13:15:23 +0200

    Added a main menu

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,17 @@ import "package:flutter/foundation.dart";
 import "package:flutter/material.dart";
 import "package:flame/game.dart";
 import "test_adventure.dart";
+import "startmenu.dart";
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   Flame.device.fullScreen();
   Flame.device.setPortrait();
 
-  TestAdventure game = TestAdventure();
-  runApp(GameWidget(game: kDebugMode ? TestAdventure() : game));
+  // Define a callback function to run the game
+  void startGame() {
+    runApp(GameWidget(game: kDebugMode ? TestAdventure() : TestAdventure()));
+  }
+
+  runApp(Startmenu(onPlayButtonPressed: startGame));
 }
