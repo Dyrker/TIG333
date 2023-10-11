8e34667d444171c91bee9d7b811ecaec800d0e77
Dyrker <elmer.blixt@gmail.com>
2023-10-11 11:09:51 +0200

    added platforms on load

--- lib/actors/platform.dart
+++ lib/actors/platform.dart
@@ -1,16 +1,18 @@
 import 'dart:async';
+import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
 import 'package:flutter_flame/test_adventure.dart';
 
-class Platform extends SpriteComponent with HasGameRef<TestAdventure>{
-
+class Platform extends SpriteComponent with HasGameRef<TestAdventure>, CollisionCallbacks{
+  int numberOfPlatforms = 5;
+  final hitbox = RectangleHitbox()..collisionType = CollisionType.passive;
   
-  Platform({position}) : super(position: position, size: Vector2(1080, 128));
+  Platform({position}) : super(position: position, size: Vector2(1080, 128)){debugMode = true;}
 
   @override
   FutureOr<void> onLoad() async{
     sprite = await Sprite.load('/Terrain/redplatform.png');
-    
+    add(hitbox);
     return super.onLoad();
   }
 }
--- lib/levels/level.dart
+++ lib/levels/level.dart
@@ -6,10 +6,10 @@ import 'package:flutter_flame/levels/background.dart';
 import 'package:flutter_flame/test_adventure.dart';
 import "package:flutter_flame/actors/platform.dart";
 
-class Level extends World with HasGameRef<TestAdventure>{
+class Level extends World with HasGameRef<TestAdventure> {
   late TiledComponent level;
   final String levelName;
-
+  final int numberOfPlatforms = 5;
 
   Player player;
 
@@ -17,10 +17,18 @@ class Level extends World with HasGameRef<TestAdventure>{
 
   @override
   FutureOr<void> onLoad() async {
-    StaticBackground level = StaticBackground(Vector2(gameRef.gameWidth,gameRef.gameHeight));
+    StaticBackground level =
+        StaticBackground(Vector2(gameRef.gameWidth, gameRef.gameHeight));
 
-    addAll([level, player, Platform(position: Vector2(0,1200))]);
+    addAll([level, player]);
 
+    for (double i = 1; i <= numberOfPlatforms; i++) {
+      add(createPlatforms(i * 0, ((i * gameRef.gameHeight/numberOfPlatforms) - 128) ));
+    }
     return super.onLoad();
+  }
+
+  Platform createPlatforms(double x, double y) {
+    return Platform(position: Vector2(x, y));
   }
 }
