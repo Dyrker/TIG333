b70560b26ef1f8e9666e56bc46ac42c880147ca5
Antpapa99 <anthony.oram@outlook.com>
2023-10-12 22:20:17 +0200

    Very basic startmenu but with a background and interactable buttons

--- lib/startmenu.dart
+++ lib/startmenu.dart
@@ -4,28 +4,38 @@ import 'package:google_fonts/google_fonts.dart';
 class Startmenu extends StatelessWidget {
   final VoidCallback onPlayButtonPressed; // Callback for the button press
 
-  Startmenu({required this.onPlayButtonPressed});
+  const Startmenu({super.key, required this.onPlayButtonPressed});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Flutter Homepage'),
-        ),
-        body: Center(
+        body: Container(
+          decoration: const BoxDecoration(
+                      image: (DecorationImage(image: AssetImage("assets/images/staticBackground.jpg"),
+                      fit: BoxFit.cover))),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Text(
-                'Hello, Flutter!',
-                style: GoogleFonts.pressStart2p()),
-              
-              SizedBox(height: 16),
               ElevatedButton(
-                onPressed: onPlayButtonPressed, // Call the callback here
-                child: Text('Play Game'),
+                onPressed: onPlayButtonPressed, 
+                child: Text(('Start Game'), style: GoogleFonts.pressStart2p())),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: onPlayButtonPressed, 
+                child: Text(('Highscores'), style: GoogleFonts.pressStart2p())),
+              
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: onPlayButtonPressed,
+                child: Text(('Select Characters'), style: GoogleFonts.pressStart2p())),
+
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: onPlayButtonPressed, 
+                child: Text(('Options'), style: GoogleFonts.pressStart2p()),
               ),
+            
             ],
           ),
         ),
