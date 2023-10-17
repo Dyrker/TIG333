49c2fe7bb43c2724503aa4627799619938637860
Antpapa99 <anthony.oram@outlook.com>
2023-10-17 21:20:31 +0200

    Starting work on providers

--- lib/jumper_api.dart
+++ lib/jumper_api.dart
@@ -1,3 +1,15 @@
+import 'package:flutter/cupertino.dart';
 import 'package:platform/platform.dart';
 import 'highscore_menu.dart';
 
+
+class JumperApi with ChangeNotifier {
+  int _platformCount = 0;
+
+  int get platformCount => _platformCount;
+
+  void updatePlatformCount(int newScore) {
+    _platformCount = newScore;
+    notifyListeners();
+  }
+}
