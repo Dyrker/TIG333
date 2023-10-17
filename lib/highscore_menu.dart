4250ed8ed819f64b1cea9dba05781fb1dbf04586
Antpapa99 <anthony.oram@outlook.com>
2023-10-17 10:44:22 +0200

    Added a high score widget

--- lib/highscore_menu.dart
+++ lib/highscore_menu.dart
@@ -1,24 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class HighScoreMenu extends StatelessWidget { // Callback for the button press
+class HighScoreMenu extends StatelessWidget {
 
   const HighScoreMenu({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return 
-      Scaffold(
+    return Scaffold(
         body: Container(
           decoration: const BoxDecoration(
                       image: (DecorationImage(image: AssetImage("assets/images/staticBackground.jpg"),
                       fit: BoxFit.cover))),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
+              _score("Hello world", "100")
             ],
           ),
         ),
       );
   }
+}
+
+Widget _score(String name, String number) {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+    Container(
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+            Text(name,
+            style: TextStyle(fontSize: 15),
+            ),
+            SizedBox(width: 10),
+            Text(number,
+            style: TextStyle(fontSize: 15)
+          )
+        ],
+      ),
+    )
+    ]
+  );
 }
